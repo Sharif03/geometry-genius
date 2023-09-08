67fc4584cbb06a0ae2b98c57d92f8fde34dd1e0a
Sharif03 <abinsu7@gmail.com>
2023-09-09 02:49:37 +0600

    six geometric area calculation completed

--- scripts/area-calculator.js
+++ scripts/area-calculator.js
@@ -1,27 +1,56 @@
 
-function calculateTriangleArea(){
-    const baseField = document.getElementById('triangle-base');
-    const baseFieldText = baseField.value;
-    const base = parseFloat(baseFieldText);
-
-    const heightField = document.getElementById('triangle-height');
-    const heightFieldText = heightField.value;
-    const height = parseFloat(heightFieldText);
-
-    const area = 0.5 * base * height;
-    const triangleAreaField = document.getElementById('triangle-area');
-    triangleAreaField.innerText = area;
+// Reusable function for get value from HTML(DOM) and set value in HTML(Dom)
+function getFieldValueById(fieldId){
+    const fieldValue = document.getElementById(fieldId);
+    const fieldValueText = fieldValue.value;
+    const value = parseFloat(fieldValueText);
+    return value;
 }
+function setElementValueById(elementId, value){
+    const element = document.getElementById(elementId);
+    const twoDecimalValue = value.toFixed(2);
+    element.innerText = twoDecimalValue;
+}
+
+//  Six(6) Geometric - Area Calculation
+function calculateTriangleArea(){
+    const base = getFieldValueById('triangle-base');
+    const height = getFieldValueById('triangle-height');
+    const area = 0.5 * base * height;
+    setElementValueById('triangle-area', area);
+}
+
 function calculateRectangleArea(){
-    const widthField = document.getElementById('rectangle-width');
-    const widthFieldText = widthField.value;
-    const width = parseFloat(widthFieldText);
-
-    const lengthField = document.getElementById('rectangle-length');
-    const lengthFieldText = lengthField.value;
-    const length = parseFloat(lengthFieldText);
-
+    const width = getFieldValueById('rectangle-width');
+    const length = getFieldValueById('rectangle-length');
     const area = width * length;
-    const rectangleAreaField = document.getElementById('rectangle-area');
-    rectangleAreaField.innerText = area;
+    setElementValueById('rectangle-area', area);
+}
+
+function calculateParallelogramArea(){
+    const base = getFieldValueById('parallelogram-base');
+    const height = getFieldValueById('parallelogram-height');
+    const area = base * height;
+    setElementValueById('parallelogram-area', area);
+}
+
+function calculateRhombusArea(){
+    const d1 = getFieldValueById('rhombus-d1');
+    const d2 = getFieldValueById('rhombus-d2');
+    const area = 0.5 * d1 * d2;
+    setElementValueById('rhombus-area', area);
+}
+
+function calculatePentagonArea(){
+    const perimeter = getFieldValueById('pentagon-perimeter');
+    const apothem = getFieldValueById('pentagon-apothem');
+    const area = 0.5 * perimeter * apothem;
+    setElementValueById('pentagon-area', area);
+}
+
+function calculateEllipseArea(){
+    const major = getFieldValueById('ellipse-major-axis');
+    const minor = getFieldValueById('ellipse-minor-axis');
+    const area = Math.PI * major * minor;
+    setElementValueById('ellipse-area', area);
 }
